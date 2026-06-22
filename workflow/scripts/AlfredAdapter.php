8d35bcdd7c458fc8578285df9319e28ac0ce2102
fradeet <41571697+fradeet@users.noreply.github.com>
2026-06-22 16:01:56 +0800

    combine Alfred function to one file.

--- workflow/scripts/AlfredAdapter.php
+++ workflow/scripts/AlfredAdapter.php
@@ -56,3 +56,37 @@ function InitRecPageAlfred(
         variables: ["cache_file_name" => $filename],
     );
 }
+
+function GetRecRowDetailAlfred(
+    string $rid,
+    string $alfred_cache_dir,
+    string $cache_file_name,
+): AlfredSF {
+    $cache_path =
+        $alfred_cache_dir .
+        DIRECTORY_SEPARATOR .
+        "Music-Recommend" .
+        DIRECTORY_SEPARATOR .
+        $cache_file_name;
+    $rec_data = file_get_contents($cache_path) |> json_decode(...);
+    $rec_row = $rec_data->resources->{"personal-recommendation"}->{$rid};
+    $rec_row_data = $rec_row->relationships->contents->data;
+    $items = [];
+    $rec_res = $rec_data->resources;
+    foreach ($rec_row_data as $item) {
+        if (
+            array_any(
+                $rec_row->attributes->resourceTypes,
+                fn($v) => ResType::tryFrom($v) !== null,
+            )
+        ) {
+            $res_type = ResType::tryFrom($item->type);
+            if ($res_type === ResType::Albums) {
+                $items[] = new AlfredSFItem(
+                    $rec_res->{$res_type->value}->{$item->id}->attributes->name,
+                );
+            }
+        }
+    }
+    return new AlfredSF($items);
+}
