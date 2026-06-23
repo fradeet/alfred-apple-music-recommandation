35dd69b10bb37f15cea87aa6537c150f0f503191
fradeet <41571697+fradeet@users.noreply.github.com>
2026-06-23 17:48:31 +0800

    Fix

--- workflow/scripts/AlfredAdapter.php
+++ workflow/scripts/AlfredAdapter.php
@@ -1,6 +1,8 @@
 <?php
+declare(strict_types=1);
 require_once __DIR__ . "/share/AppleMusic.php";
 require_once __DIR__ . "/AlfredSFType/AlfredScriptFilterType.php";
+
 
 /** Define a custom error handler to avoid warning messages break result extraction in Alfred */
 set_error_handler(function ($severity, $message, $file, $line) {
